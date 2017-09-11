0126cd75fc372f66949edbbb149b3c1fc03a1ca6
margarita_zargaryan <margarita_zargaryan@edu.aua.am>
2017-09-11 14:01:27 +0400

    first commit

---
+++ homework2.js
@@ -0,0 +1,31 @@
+
+const age=17;
+
+if ( age> 17 ){
+	console.log ( "You are able to work" )
+} else{
+	console.log( "Go and ask money from your parents !" );
+}
+const name = "Varazdat";
+if (name === 'Varazdat'){
+		console.log ("Hey, Hi Varazdat!")
+	} else {
+		console.log ("Sorry! You are not Varazdat");
+	}
+	
+var x=7;
+var y=5;
+var sum= x+y;
+var sub= x-5;
+console.log (sum);
+console.log (sub);
+var a=14
+var b=25
+var mul= a*b;
+var div= a/b;
+console.log (mul);
+console.log (div);
+
+		
+		
+		
